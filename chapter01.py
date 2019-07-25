cade1735441793a574314e60ad735a79b7a785b8
Sanghyun <sanghyun.lee8209@gmail.com>
2019-07-26 00:02:46 +0900

    chapter1
    chapter1

---
+++ chapter01.py
@@ -0,0 +1,15 @@
+print("Python Programming")
+import keyword
+print(keyword.kwlist)
+
+print("대문자로 시작하는 캐멀케이스는 클래스")
+print("스네이크 케이스에 뒤에 괄호 있으면 함수, 괄호 없으며 변수")
+
+print(52)
+print(52,273,"Hello")
+
+
+
+
+
+
---
+++ output.py
@@ -0,0 +1,18 @@
+# 하나만 
+print("# only one")
+print("Hello...PG..!")
+print(
+)
+
+# 복수
+print("# 여럿")
+print(10, 20, 30, 40)
+print("a", "b", "c")
+print()
+
+# null
+print("#노입력")
+print("--line--")
+print()
+print()
+print("--line--")
